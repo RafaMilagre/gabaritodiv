6803c1c07fae1c65514110e524761dc3b4fa349d
Rafael Milagre <rafaelmilagrearquiteto@gmail.com>
2025-06-26 22:23:03 -0300

    base

--- gabaritodiv.py
+++ gabaritodiv.py
@@ -1,2 +1,13 @@
-var = 'Hello world'
-print(var)
+print('='*40)
+print(f'{'CÁLCULO DE PARÂMETROS URBANISTÍCOS':^40}')
+print(f'{'Construído por RAFAEL MILAGRE':^40}')
+print(f'{'Cidade: Divinópolis/MG':^40}')
+print('='*40)
+
+pergunta = int(input('''Qual gabarito deseja calcular? 
+
+[ 1 ] - GABARITO FRONTAL
+
+[ 2 ] - GABARITO LATERAL 
+
+[ 3 ] - GABARITO DE FUNDO'''))
